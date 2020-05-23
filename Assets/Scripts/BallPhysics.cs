7ddf6dd10d02eb861934cbd3b78f2878cc3fef2a
Christos Tzastas <ctzastas@gmail.com>
2020-05-23 18:08:12 +0300

    Import project on github

---
+++ Assets/Scripts/BallPhysics.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+public class BallPhysics : MonoBehaviour {
+    
+    private float power = 3f;
+    private float radius = 2f;
+    private float speed = 700f;
+    private float explosionSpeed = 100f;
+    private float rotSpeed = 200f;
+    private Rigidbody ball;                                
+    [SerializeField] LayerMask explosionLayers;
+
+    // Start is called before the first frame update
+    void Start() {
+        ball = GetComponent<Rigidbody>();
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        Vector3 explosionPos = transform.position;
+        Collider[] colliders = Physics.OverlapSphere(explosionPos, radius, explosionLayers);
+        foreach (Collider hit in colliders) {
+            ball = hit.GetComponent<Rigidbody>();
+            if (ball != null) {
+                ball.AddExplosionForce(power, explosionPos, radius, 1f, ForceMode.Impulse);
+            }
+        }
+    }
+
+    void OnCollisionStay(Collision collision) {
+        if (collision.collider.gameObject.CompareTag("Platform")) {
+            ball.AddForce(Vector3.right * speed, ForceMode.Force);
+        }
+        if (collision.collider.gameObject.CompareTag("Platform3")) {
+            ball.AddTorque(Vector3.forward * rotSpeed, ForceMode.Impulse);
+        }
+    }
+    
+    void OnCollisionExit(Collision collision) {
+        if (collision.collider.gameObject.CompareTag("Platform2")) {
+            ball.AddForce(Vector3.left * speed, ForceMode.Force);
+        }
+    }
+
+    void OnTriggerStay(Collider other) {
+        if (other.gameObject.CompareTag("Wall")) {
+            ball.AddTorque(Vector3.back * rotSpeed, ForceMode.Acceleration);
+        }
+    }
+
+    void OnTriggerExit(Collider other) {
+        if (other.gameObject.CompareTag("Wall")) {
+            ball.AddForce(Vector3.right * explosionSpeed, ForceMode.Impulse);
+        }
+    }
+}
---
+++ Assets/Scripts/PlatformRaycast.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class PlatformRaycast : MonoBehaviour {
+    
+    private float minHeight = 2.5f;
+    private float forceBoost = 50f;
+    private Rigidbody platform;
+
+    // Start is called before the first frame update
+    void Start() {
+        platform = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
+       CheckHeight();
+    }
+
+    void CheckHeight() {
+        RaycastHit hit;
+        Ray distance = new Ray (transform.position, Vector3.down);
+        Debug.DrawRay (transform.position, Vector3.down, Color.red);
+        if (Physics.Raycast (distance, out hit, minHeight)) {
+            if (hit.collider != null) {
+                platform.AddForce (Vector3.up * forceBoost);
+            }
+        }
+    }
+}
